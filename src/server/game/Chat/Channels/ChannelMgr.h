Naios/TrinityCore

Core/Game: Converted the game library to a shared library. * There is still the possibility to static link against game.

src/server/game/Chat/Channels/ChannelMgr.h
@@ -24,7 +24,7 @@
 #define MAX_CHANNEL_NAME_STR 0x31
 #define MAX_CHANNEL_PASS_STR 31
 
-class ChannelMgr
+class TRINITY_GAME_API ChannelMgr
 {
     typedef std::map<std::wstring, Channel*> ChannelMap;
 

src/server/game/Conditions/ConditionMgr.h
@@ -178,7 +178,7 @@ enum MaxConditionTargets
     MAX_CONDITION_TARGETS = 3
 };
 
-struct ConditionSourceInfo
+struct TRINITY_GAME_API ConditionSourceInfo
 {
     WorldObject* mConditionTargets[MAX_CONDITION_TARGETS]; // an array of targets available for conditions
     Condition const* mLastFailedCondition;
@@ -191,7 +191,7 @@ struct ConditionSourceInfo
     }
 };
 
-struct Condition
+struct TRINITY_GAME_API Condition
 {
     ConditionSourceType     SourceType;        //SourceTypeOrReferenceId
     uint32                  SourceGroup;
@@ -243,7 +243,7 @@ typedef std::unordered_map<uint32, ConditionsByEntryMap> ConditionEntriesByCreat
 typedef std::unordered_map<std::pair<int32, uint32 /*SAI source_type*/>, ConditionsByEntryMap> SmartEventConditionContainer;
 typedef std::unordered_map<uint32, ConditionContainer> ConditionReferenceContainer;//only used for references
 
-class ConditionMgr
+class TRINITY_GAME_API ConditionMgr
 {
     private:
         ConditionMgr();

src/server/game/Scripting/ScriptMgr.h
@@ -146,7 +146,7 @@ struct OutdoorPvPData;
     event on all registered scripts of that type.
 */
 
-class ScriptObject
+class TRINITY_GAME_API ScriptObject
 {
     friend class ScriptMgr;
 
@@ -185,7 +185,7 @@ template<class TObject> class UpdatableScript
         virtual void OnUpdate(TObject* /*obj*/, uint32 /*diff*/) { }
 };
 
-class SpellScriptLoader : public ScriptObject
+class TRINITY_GAME_API SpellScriptLoader : public ScriptObject
 {
     protected:
 
@@ -200,7 +200,7 @@ class SpellScriptLoader : public ScriptObject
         virtual AuraScript* GetAuraScript() const { return NULL; }
 };
 
-class ServerScript : public ScriptObject
+class TRINITY_GAME_API ServerScript : public ScriptObject
 {
     protected:
 
@@ -334,14 +334,14 @@ template<class TMap> class MapScript : public UpdatableScript<TMap>
         virtual void OnPlayerLeave(TMap* /*map*/, Player* /*player*/) { }
 };
 
-class WorldMapScript : public ScriptObject, public MapScript<Map>
+class TRINITY_GAME_API WorldMapScript : public ScriptObject, public MapScript<Map>
 {
     protected:
 
         WorldMapScript(const char* name, uint32 mapId);
 };
 
-class InstanceMapScript : public ScriptObject, public MapScript<InstanceMap>
+class TRINITY_GAME_API InstanceMapScript : public ScriptObject, public MapScript<InstanceMap>
 {
     protected:
 
@@ -353,14 +353,14 @@ class InstanceMapScript : public ScriptObject, public MapScript<InstanceMap>
         virtual InstanceScript* GetInstanceScript(InstanceMap* /*map*/) const { return NULL; }
 };
 
-class BattlegroundMapScript : public ScriptObject, public MapScript<BattlegroundMap>
+class TRINITY_GAME_API BattlegroundMapScript : public ScriptObject, public MapScript<BattlegroundMap>
 {
     protected:
 
         BattlegroundMapScript(const char* name, uint32 mapId);
 };
 
-class ItemScript : public ScriptObject
+class TRINITY_GAME_API ItemScript : public ScriptObject
 {
     protected:
 
@@ -384,7 +384,7 @@ class ItemScript : public ScriptObject
         virtual bool OnRemove(Player* /*player*/, Item* /*item*/) { return false; }
 };
 
-class UnitScript : public ScriptObject
+class TRINITY_GAME_API UnitScript : public ScriptObject
 {
     protected:
 
@@ -407,7 +407,7 @@ class UnitScript : public ScriptObject
         virtual void ModifySpellDamageTaken(Unit* /*target*/, Unit* /*attacker*/, int32& /*damage*/) { }
 };
 
-class CreatureScript : public UnitScript, public UpdatableScript<Creature>
+class TRINITY_GAME_API CreatureScript : public UnitScript, public UpdatableScript<Creature>
 {
     protected:
 
@@ -443,7 +443,7 @@ class CreatureScript : public UnitScript, public UpdatableScript<Creature>
         virtual CreatureAI* GetAI(Creature* /*creature*/) const { return NULL; }
 };
 
-class GameObjectScript : public ScriptObject, public UpdatableScript<GameObject>
+class TRINITY_GAME_API GameObjectScript : public ScriptObject, public UpdatableScript<GameObject>
 {
     protected:
 
@@ -488,7 +488,7 @@ class GameObjectScript : public ScriptObject, public UpdatableScript<GameObject>
         virtual GameObjectAI* GetAI(GameObject* /*go*/) const { return NULL; }
 };
 
-class AreaTriggerScript : public ScriptObject
+class TRINITY_GAME_API AreaTriggerScript : public ScriptObject
 {
     protected:
 
@@ -500,7 +500,7 @@ class AreaTriggerScript : public ScriptObject
         virtual bool OnTrigger(Player* /*player*/, AreaTriggerEntry const* /*trigger*/, bool /*entered*/) { return false; }
 };
 
-class BattlegroundScript : public ScriptObject
+class TRINITY_GAME_API BattlegroundScript : public ScriptObject
 {
     protected:
 
@@ -512,7 +512,7 @@ class BattlegroundScript : public ScriptObject
         virtual Battleground* GetBattleground() const = 0;
 };
 
-class OutdoorPvPScript : public ScriptObject
+class TRINITY_GAME_API OutdoorPvPScript : public ScriptObject
 {
     protected:
 
@@ -524,7 +524,7 @@ class OutdoorPvPScript : public ScriptObject
         virtual OutdoorPvP* GetOutdoorPvP() const = 0;
 };
 
-class CommandScript : public ScriptObject
+class TRINITY_GAME_API CommandScript : public ScriptObject
 {
     protected:
 
@@ -536,7 +536,7 @@ class CommandScript : public ScriptObject
         virtual std::vector<ChatCommand> GetCommands() const = 0;
 };
 
-class WeatherScript : public ScriptObject, public UpdatableScript<Weather>
+class TRINITY_GAME_API WeatherScript : public ScriptObject, public UpdatableScript<Weather>
 {
     protected:
 
@@ -548,7 +548,7 @@ class WeatherScript : public ScriptObject, public UpdatableScript<Weather>
         virtual void OnChange(Weather* /*weather*/, WeatherState /*state*/, float /*grade*/) { }
 };
 
-class AuctionHouseScript : public ScriptObject
+class TRINITY_GAME_API AuctionHouseScript : public ScriptObject
 {
     protected:
 
@@ -569,7 +569,7 @@ class AuctionHouseScript : public ScriptObject
         virtual void OnAuctionExpire(AuctionHouseObject* /*ah*/, AuctionEntry* /*entry*/) { }
 };
 
-class ConditionScript : public ScriptObject
+class TRINITY_GAME_API ConditionScript : public ScriptObject
 {
     protected:
 
@@ -581,7 +581,7 @@ class ConditionScript : public ScriptObject
         virtual bool OnConditionCheck(Condition const* /*condition*/, ConditionSourceInfo& /*sourceInfo*/) { return true; }
 };
 
-class VehicleScript : public ScriptObject
+class TRINITY_GAME_API VehicleScript : public ScriptObject
 {
     protected:
 
@@ -608,14 +608,14 @@ class VehicleScript : public ScriptObject
         virtual void OnRemovePassenger(Vehicle* /*veh*/, Unit* /*passenger*/) { }
 };
 
-class DynamicObjectScript : public ScriptObject, public UpdatableScript<DynamicObject>
+class TRINITY_GAME_API DynamicObjectScript : public ScriptObject, public UpdatableScript<DynamicObject>
 {
     protected:
 
         DynamicObjectScript(const char* name);
 };
 
-class TransportScript : public ScriptObject, public UpdatableScript<Transport>
+class TRINITY_GAME_API TransportScript : public ScriptObject, public UpdatableScript<Transport>
 {
     protected:
 
@@ -636,7 +636,7 @@ class TransportScript : public ScriptObject, public UpdatableScript<Transport>
         virtual void OnRelocate(Transport* /*transport*/, uint32 /*waypointId*/, uint32 /*mapId*/, float /*x*/, float /*y*/, float /*z*/) { }
 };
 
-class AchievementCriteriaScript : public ScriptObject
+class TRINITY_GAME_API AchievementCriteriaScript : public ScriptObject
 {
     protected:
 
@@ -648,7 +648,7 @@ class AchievementCriteriaScript : public ScriptObject
         virtual bool OnCheck(Player* source, Unit* target) = 0;
 };
 
-class PlayerScript : public UnitScript
+class TRINITY_GAME_API PlayerScript : public UnitScript
 {
     protected:
 
@@ -745,7 +745,7 @@ class PlayerScript : public UnitScript
         virtual void OnQuestStatusChange(Player* /*player*/, uint32 /*questId*/, QuestStatus /*status*/) { }
 };
 
-class AccountScript : public ScriptObject
+class TRINITY_GAME_API AccountScript : public ScriptObject
 {
     protected:
 
@@ -772,7 +772,7 @@ class AccountScript : public ScriptObject
         virtual void OnFailedPasswordChange(uint32 /*accountId*/) {}
 };
 
-class GuildScript : public ScriptObject
+class TRINITY_GAME_API GuildScript : public ScriptObject
 {
     protected:
 
@@ -813,7 +813,7 @@ class GuildScript : public ScriptObject
         virtual void OnBankEvent(Guild* /*guild*/, uint8 /*eventType*/, uint8 /*tabId*/, ObjectGuid::LowType /*playerGuid*/, uint64 /*itemOrMoney*/, uint16 /*itemStackCount*/, uint8 /*destTabId*/) { }
 };
 
-class GroupScript : public ScriptObject
+class TRINITY_GAME_API GroupScript : public ScriptObject
 {
     protected:
 
@@ -843,11 +843,11 @@ class GroupScript : public ScriptObject
 // namespace
 // {
     typedef std::list<std::string> UnusedScriptNamesContainer;
-    extern UnusedScriptNamesContainer UnusedScriptNames;
+    TRINITY_GAME_API extern UnusedScriptNamesContainer UnusedScriptNames;
 // }
 
 // Manages registration, loading, and execution of scripts.
-class ScriptMgr
+class TRINITY_GAME_API ScriptMgr
 {
     friend class ScriptObject;
 

src/server/game/Scripting/ScriptSystem.h
@@ -59,7 +59,7 @@ struct ScriptPointMove
 
 typedef std::vector<ScriptPointMove> ScriptPointVector;
 
-class SystemMgr
+class TRINITY_GAME_API SystemMgr
 {
     private:
         SystemMgr() { }

src/server/game/Server/Protocol/PacketLog.h
@@ -32,7 +32,7 @@ enum Direction
 
 class WorldPacket;
 
-class PacketLog
+class TRINITY_GAME_API PacketLog
 {
     private:
         PacketLog();

src/server/game/Support/SupportMgr.cpp
@@ -409,6 +409,10 @@ SuggestionTicket* SupportMgr::GetTicket<SuggestionTicket>(uint32 suggestionId)
 
 }
 
+template TRINITY_GAME_API BugTicket* SupportMgr::GetTicket<BugTicket>(uint32);
+template TRINITY_GAME_API ComplaintTicket* SupportMgr::GetTicket<ComplaintTicket>(uint32);
+template TRINITY_GAME_API SuggestionTicket* SupportMgr::GetTicket<SuggestionTicket>(uint32);
+
 template<>
 uint32 SupportMgr::GetOpenTicketCount<BugTicket>() const { return _openBugTicketCount; }
 
@@ -417,6 +421,10 @@ uint32 SupportMgr::GetOpenTicketCount<ComplaintTicket>() const { return _openCom
 
 template<>
 uint32 SupportMgr::GetOpenTicketCount<SuggestionTicket>() const { return _openSuggestionTicketCount; }
+
+template TRINITY_GAME_API uint32 SupportMgr::GetOpenTicketCount<BugTicket>() const;
+template TRINITY_GAME_API uint32 SupportMgr::GetOpenTicketCount<ComplaintTicket>() const;
+template TRINITY_GAME_API uint32 SupportMgr::GetOpenTicketCount<SuggestionTicket>() const;
 
 void SupportMgr::LoadBugTickets()
 {
@@ -613,6 +621,10 @@ void SupportMgr::RemoveTicket<SuggestionTicket>(uint32 ticketId)
     }
 }
 
+template TRINITY_GAME_API void SupportMgr::RemoveTicket<BugTicket>(uint32);
+template TRINITY_GAME_API void SupportMgr::RemoveTicket<ComplaintTicket>(uint32);
+template TRINITY_GAME_API void SupportMgr::RemoveTicket<SuggestionTicket>(uint32);
+
 template<>
 void SupportMgr::CloseTicket<BugTicket>(uint32 ticketId, ObjectGuid closedBy)
 {
@@ -648,6 +660,10 @@ void SupportMgr::CloseTicket<SuggestionTicket>(uint32 ticketId, ObjectGuid close
         ticket->SaveToDB();
     }
 }
+
+template TRINITY_GAME_API void SupportMgr::CloseTicket<BugTicket>(uint32, ObjectGuid);
+template TRINITY_GAME_API void SupportMgr::CloseTicket<ComplaintTicket>(uint32, ObjectGuid);
+template TRINITY_GAME_API void SupportMgr::CloseTicket<SuggestionTicket>(uint32, ObjectGuid);
 
 template<>
 void SupportMgr::ResetTickets<BugTicket>()
@@ -690,6 +706,10 @@ void SupportMgr::ResetTickets<SuggestionTicket>()
     CharacterDatabase.Execute(stmt);
 }
 
+template TRINITY_GAME_API void SupportMgr::ResetTickets<BugTicket>();
+template TRINITY_GAME_API void SupportMgr::ResetTickets<ComplaintTicket>();
+template TRINITY_GAME_API void SupportMgr::ResetTickets<SuggestionTicket>();
+
 template<>
 void SupportMgr::ShowList<BugTicket>(ChatHandler& handler) const
 {
@@ -717,6 +737,10 @@ void SupportMgr::ShowList<SuggestionTicket>(ChatHandler& handler) const
             handler.SendSysMessage(itr->second->FormatViewMessageString(handler).c_str());
 }
 
+template TRINITY_GAME_API void SupportMgr::ShowList<BugTicket>(ChatHandler&) const;
+template TRINITY_GAME_API void SupportMgr::ShowList<ComplaintTicket>(ChatHandler&) const;
+template TRINITY_GAME_API void SupportMgr::ShowList<SuggestionTicket>(ChatHandler&) const;
+
 template<>
 void SupportMgr::ShowClosedList<BugTicket>(ChatHandler& handler) const
 {
@@ -743,3 +767,7 @@ void SupportMgr::ShowClosedList<SuggestionTicket>(ChatHandler& handler) const
         if (itr->second->IsClosed())
             handler.SendSysMessage(itr->second->FormatViewMessageString(handler).c_str());
 }
+
+template TRINITY_GAME_API void SupportMgr::ShowClosedList<BugTicket>(ChatHandler&) const;
+template TRINITY_GAME_API void SupportMgr::ShowClosedList<ComplaintTicket>(ChatHandler&) const;
+template TRINITY_GAME_API void SupportMgr::ShowClosedList<SuggestionTicket>(ChatHandler&) const;

src/server/game/Texts/CreatureTextMgr.h
@@ -76,7 +76,7 @@ typedef std::unordered_map<uint32, CreatureTextHolder> CreatureTextMap;     // a
 
 typedef std::map<CreatureTextId, CreatureTextLocale> LocaleCreatureTextMap;
 
-class CreatureTextMgr
+class TRINITY_GAME_API CreatureTextMgr
 {
     private:
         CreatureTextMgr() { }

allankrdec/TrinityCore-10-2024

src/server/game/Chat/Channels/ChannelMgr.h
@@ -24,7 +24,7 @@
 #define MAX_CHANNEL_NAME_STR 0x31
 #define MAX_CHANNEL_PASS_STR 31
 
-class ChannelMgr
+class TC_GAME_API ChannelMgr
 {
     typedef std::map<std::wstring, Channel*> ChannelMap;
 

src/server/game/Conditions/ConditionMgr.h
@@ -178,7 +178,7 @@ enum MaxConditionTargets
     MAX_CONDITION_TARGETS = 3
 };
 
-struct ConditionSourceInfo
+struct TC_GAME_API ConditionSourceInfo
 {
     WorldObject* mConditionTargets[MAX_CONDITION_TARGETS]; // an array of targets available for conditions
     Condition const* mLastFailedCondition;
@@ -191,7 +191,7 @@ struct ConditionSourceInfo
     }
 };
 
-struct Condition
+struct TC_GAME_API Condition
 {
     ConditionSourceType     SourceType;        //SourceTypeOrReferenceId
     uint32                  SourceGroup;
@@ -243,7 +243,7 @@ typedef std::unordered_map<uint32, ConditionsByEntryMap> ConditionEntriesByCreat
 typedef std::unordered_map<std::pair<int32, uint32 /*SAI source_type*/>, ConditionsByEntryMap> SmartEventConditionContainer;
 typedef std::unordered_map<uint32, ConditionContainer> ConditionReferenceContainer;//only used for references
 
-class ConditionMgr
+class TC_GAME_API ConditionMgr
 {
     private:
         ConditionMgr();

src/server/game/Scripting/ScriptMgr.h
@@ -146,7 +146,7 @@ struct OutdoorPvPData;
     event on all registered scripts of that type.
 */
 
-class ScriptObject
+class TC_GAME_API ScriptObject
 {
     friend class ScriptMgr;
 
@@ -185,7 +185,7 @@ template<class TObject> class UpdatableScript
         virtual void OnUpdate(TObject* /*obj*/, uint32 /*diff*/) { }
 };
 
-class SpellScriptLoader : public ScriptObject
+class TC_GAME_API SpellScriptLoader : public ScriptObject
 {
     protected:
 
@@ -200,7 +200,7 @@ class SpellScriptLoader : public ScriptObject
         virtual AuraScript* GetAuraScript() const { return NULL; }
 };
 
-class ServerScript : public ScriptObject
+class TC_GAME_API ServerScript : public ScriptObject
 {
     protected:
 
@@ -330,14 +330,14 @@ template<class TMap> class MapScript : public UpdatableScript<TMap>
         virtual void OnPlayerLeave(TMap* /*map*/, Player* /*player*/) { }
 };
 
-class WorldMapScript : public ScriptObject, public MapScript<Map>
+class TC_GAME_API WorldMapScript : public ScriptObject, public MapScript<Map>
 {
     protected:
 
         WorldMapScript(const char* name, uint32 mapId);
 };
 
-class InstanceMapScript : public ScriptObject, public MapScript<InstanceMap>
+class TC_GAME_API InstanceMapScript : public ScriptObject, public MapScript<InstanceMap>
 {
     protected:
 
@@ -349,14 +349,14 @@ class InstanceMapScript : public ScriptObject, public MapScript<InstanceMap>
         virtual InstanceScript* GetInstanceScript(InstanceMap* /*map*/) const { return NULL; }
 };
 
-class BattlegroundMapScript : public ScriptObject, public MapScript<BattlegroundMap>
+class TC_GAME_API BattlegroundMapScript : public ScriptObject, public MapScript<BattlegroundMap>
 {
     protected:
 
         BattlegroundMapScript(const char* name, uint32 mapId);
 };
 
-class ItemScript : public ScriptObject
+class TC_GAME_API ItemScript : public ScriptObject
 {
     protected:
 
@@ -380,7 +380,7 @@ class ItemScript : public ScriptObject
         virtual bool OnRemove(Player* /*player*/, Item* /*item*/) { return false; }
 };
 
-class UnitScript : public ScriptObject
+class TC_GAME_API UnitScript : public ScriptObject
 {
     protected:
 
@@ -403,7 +403,7 @@ class UnitScript : public ScriptObject
         virtual void ModifySpellDamageTaken(Unit* /*target*/, Unit* /*attacker*/, int32& /*damage*/) { }
 };
 
-class CreatureScript : public UnitScript, public UpdatableScript<Creature>
+class TC_GAME_API CreatureScript : public UnitScript, public UpdatableScript<Creature>
 {
     protected:
 
@@ -439,7 +439,7 @@ class CreatureScript : public UnitScript, public UpdatableScript<Creature>
         virtual CreatureAI* GetAI(Creature* /*creature*/) const { return NULL; }
 };
 
-class GameObjectScript : public ScriptObject, public UpdatableScript<GameObject>
+class TC_GAME_API GameObjectScript : public ScriptObject, public UpdatableScript<GameObject>
 {
     protected:
 
@@ -484,7 +484,7 @@ class GameObjectScript : public ScriptObject, public UpdatableScript<GameObject>
         virtual GameObjectAI* GetAI(GameObject* /*go*/) const { return NULL; }
 };
 
-class AreaTriggerScript : public ScriptObject
+class TC_GAME_API AreaTriggerScript : public ScriptObject
 {
     protected:
 
@@ -496,7 +496,7 @@ class AreaTriggerScript : public ScriptObject
         virtual bool OnTrigger(Player* /*player*/, AreaTriggerEntry const* /*trigger*/, bool /*entered*/) { return false; }
 };
 
-class BattlegroundScript : public ScriptObject
+class TC_GAME_API BattlegroundScript : public ScriptObject
 {
     protected:
 
@@ -508,7 +508,7 @@ class BattlegroundScript : public ScriptObject
         virtual Battleground* GetBattleground() const = 0;
 };
 
-class OutdoorPvPScript : public ScriptObject
+class TC_GAME_API OutdoorPvPScript : public ScriptObject
 {
     protected:
 
@@ -520,7 +520,7 @@ class OutdoorPvPScript : public ScriptObject
         virtual OutdoorPvP* GetOutdoorPvP() const = 0;
 };
 
-class CommandScript : public ScriptObject
+class TC_GAME_API CommandScript : public ScriptObject
 {
     protected:
 
@@ -532,7 +532,7 @@ class CommandScript : public ScriptObject
         virtual std::vector<ChatCommand> GetCommands() const = 0;
 };
 
-class WeatherScript : public ScriptObject, public UpdatableScript<Weather>
+class TC_GAME_API WeatherScript : public ScriptObject, public UpdatableScript<Weather>
 {
     protected:
 
@@ -544,7 +544,7 @@ class WeatherScript : public ScriptObject, public UpdatableScript<Weather>
         virtual void OnChange(Weather* /*weather*/, WeatherState /*state*/, float /*grade*/) { }
 };
 
-class AuctionHouseScript : public ScriptObject
+class TC_GAME_API AuctionHouseScript : public ScriptObject
 {
     protected:
 
@@ -565,7 +565,7 @@ class AuctionHouseScript : public ScriptObject
         virtual void OnAuctionExpire(AuctionHouseObject* /*ah*/, AuctionEntry* /*entry*/) { }
 };
 
-class ConditionScript : public ScriptObject
+class TC_GAME_API ConditionScript : public ScriptObject
 {
     protected:
 
@@ -577,7 +577,7 @@ class ConditionScript : public ScriptObject
         virtual bool OnConditionCheck(Condition const* /*condition*/, ConditionSourceInfo& /*sourceInfo*/) { return true; }
 };
 
-class VehicleScript : public ScriptObject
+class TC_GAME_API VehicleScript : public ScriptObject
 {
     protected:
 
@@ -604,14 +604,14 @@ class VehicleScript : public ScriptObject
         virtual void OnRemovePassenger(Vehicle* /*veh*/, Unit* /*passenger*/) { }
 };
 
-class DynamicObjectScript : public ScriptObject, public UpdatableScript<DynamicObject>
+class TC_GAME_API DynamicObjectScript : public ScriptObject, public UpdatableScript<DynamicObject>
 {
     protected:
 
         DynamicObjectScript(const char* name);
 };
 
-class TransportScript : public ScriptObject, public UpdatableScript<Transport>
+class TC_GAME_API TransportScript : public ScriptObject, public UpdatableScript<Transport>
 {
     protected:
 
@@ -632,7 +632,7 @@ class TransportScript : public ScriptObject, public UpdatableScript<Transport>
         virtual void OnRelocate(Transport* /*transport*/, uint32 /*waypointId*/, uint32 /*mapId*/, float /*x*/, float /*y*/, float /*z*/) { }
 };
 
-class AchievementCriteriaScript : public ScriptObject
+class TC_GAME_API AchievementCriteriaScript : public ScriptObject
 {
     protected:
 
@@ -644,7 +644,7 @@ class AchievementCriteriaScript : public ScriptObject
         virtual bool OnCheck(Player* source, Unit* target) = 0;
 };
 
-class PlayerScript : public UnitScript
+class TC_GAME_API PlayerScript : public UnitScript
 {
     protected:
 
@@ -741,7 +741,7 @@ class PlayerScript : public UnitScript
         virtual void OnQuestStatusChange(Player* /*player*/, uint32 /*questId*/, QuestStatus /*status*/) { }
 };
 
-class AccountScript : public ScriptObject
+class TC_GAME_API AccountScript : public ScriptObject
 {
     protected:
 
@@ -768,7 +768,7 @@ class AccountScript : public ScriptObject
         virtual void OnFailedPasswordChange(uint32 /*accountId*/) {}
 };
 
-class GuildScript : public ScriptObject
+class TC_GAME_API GuildScript : public ScriptObject
 {
     protected:
 
@@ -809,7 +809,7 @@ class GuildScript : public ScriptObject
         virtual void OnBankEvent(Guild* /*guild*/, uint8 /*eventType*/, uint8 /*tabId*/, ObjectGuid::LowType /*playerGuid*/, uint64 /*itemOrMoney*/, uint16 /*itemStackCount*/, uint8 /*destTabId*/) { }
 };
 
-class GroupScript : public ScriptObject
+class TC_GAME_API GroupScript : public ScriptObject
 {
     protected:
 
@@ -839,11 +839,11 @@ class GroupScript : public ScriptObject
 // namespace
 // {
     typedef std::list<std::string> UnusedScriptNamesContainer;
-    extern UnusedScriptNamesContainer UnusedScriptNames;
+    TC_GAME_API extern UnusedScriptNamesContainer UnusedScriptNames;
 // }
 
 // Manages registration, loading, and execution of scripts.
-class ScriptMgr
+class TC_GAME_API ScriptMgr
 {
     friend class ScriptObject;
 

src/server/game/Scripting/ScriptSystem.h
@@ -59,7 +59,7 @@ struct ScriptPointMove
 
 typedef std::vector<ScriptPointMove> ScriptPointVector;
 
-class SystemMgr
+class TC_GAME_API SystemMgr
 {
     private:
         SystemMgr() { }

src/server/game/Server/Protocol/PacketLog.h
@@ -32,7 +32,7 @@ enum Direction
 
 class WorldPacket;
 
-class PacketLog
+class TC_GAME_API PacketLog
 {
     private:
         PacketLog();

src/server/game/Support/SupportMgr.cpp
@@ -409,6 +409,10 @@ SuggestionTicket* SupportMgr::GetTicket<SuggestionTicket>(uint32 suggestionId)
 
 }
 
+template TC_GAME_API BugTicket* SupportMgr::GetTicket<BugTicket>(uint32);
+template TC_GAME_API ComplaintTicket* SupportMgr::GetTicket<ComplaintTicket>(uint32);
+template TC_GAME_API SuggestionTicket* SupportMgr::GetTicket<SuggestionTicket>(uint32);
+
 template<>
 uint32 SupportMgr::GetOpenTicketCount<BugTicket>() const { return _openBugTicketCount; }
 
@@ -417,6 +421,10 @@ uint32 SupportMgr::GetOpenTicketCount<ComplaintTicket>() const { return _openCom
 
 template<>
 uint32 SupportMgr::GetOpenTicketCount<SuggestionTicket>() const { return _openSuggestionTicketCount; }
+
+template TC_GAME_API uint32 SupportMgr::GetOpenTicketCount<BugTicket>() const;
+template TC_GAME_API uint32 SupportMgr::GetOpenTicketCount<ComplaintTicket>() const;
+template TC_GAME_API uint32 SupportMgr::GetOpenTicketCount<SuggestionTicket>() const;
 
 void SupportMgr::LoadBugTickets()
 {
@@ -613,6 +621,10 @@ void SupportMgr::RemoveTicket<SuggestionTicket>(uint32 ticketId)
     }
 }
 
+template TC_GAME_API void SupportMgr::RemoveTicket<BugTicket>(uint32);
+template TC_GAME_API void SupportMgr::RemoveTicket<ComplaintTicket>(uint32);
+template TC_GAME_API void SupportMgr::RemoveTicket<SuggestionTicket>(uint32);
+
 template<>
 void SupportMgr::CloseTicket<BugTicket>(uint32 ticketId, ObjectGuid closedBy)
 {
@@ -648,6 +660,10 @@ void SupportMgr::CloseTicket<SuggestionTicket>(uint32 ticketId, ObjectGuid close
         ticket->SaveToDB();
     }
 }
+
+template TC_GAME_API void SupportMgr::CloseTicket<BugTicket>(uint32, ObjectGuid);
+template TC_GAME_API void SupportMgr::CloseTicket<ComplaintTicket>(uint32, ObjectGuid);
+template TC_GAME_API void SupportMgr::CloseTicket<SuggestionTicket>(uint32, ObjectGuid);
 
 template<>
 void SupportMgr::ResetTickets<BugTicket>()
@@ -690,6 +706,10 @@ void SupportMgr::ResetTickets<SuggestionTicket>()
     CharacterDatabase.Execute(stmt);
 }
 
+template TC_GAME_API void SupportMgr::ResetTickets<BugTicket>();
+template TC_GAME_API void SupportMgr::ResetTickets<ComplaintTicket>();
+template TC_GAME_API void SupportMgr::ResetTickets<SuggestionTicket>();
+
 template<>
 void SupportMgr::ShowList<BugTicket>(ChatHandler& handler) const
 {
@@ -717,6 +737,10 @@ void SupportMgr::ShowList<SuggestionTicket>(ChatHandler& handler) const
             handler.SendSysMessage(itr->second->FormatViewMessageString(handler).c_str());
 }
 
+template TC_GAME_API void SupportMgr::ShowList<BugTicket>(ChatHandler&) const;
+template TC_GAME_API void SupportMgr::ShowList<ComplaintTicket>(ChatHandler&) const;
+template TC_GAME_API void SupportMgr::ShowList<SuggestionTicket>(ChatHandler&) const;
+
 template<>
 void SupportMgr::ShowClosedList<BugTicket>(ChatHandler& handler) const
 {
@@ -743,3 +767,7 @@ void SupportMgr::ShowClosedList<SuggestionTicket>(ChatHandler& handler) const
         if (itr->second->IsClosed())
             handler.SendSysMessage(itr->second->FormatViewMessageString(handler).c_str());
 }
+
+template TC_GAME_API void SupportMgr::ShowClosedList<BugTicket>(ChatHandler&) const;
+template TC_GAME_API void SupportMgr::ShowClosedList<ComplaintTicket>(ChatHandler&) const;
+template TC_GAME_API void SupportMgr::ShowClosedList<SuggestionTicket>(ChatHandler&) const;

src/server/game/Texts/CreatureTextMgr.h
@@ -76,7 +76,7 @@ typedef std::unordered_map<uint32, CreatureTextHolder> CreatureTextMap;     // a
 
 typedef std::map<CreatureTextId, CreatureTextLocale> LocaleCreatureTextMap;
 
-class CreatureTextMgr
+class TC_GAME_API CreatureTextMgr
 {
     private:
         CreatureTextMgr() { }