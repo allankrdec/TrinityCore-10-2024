TrinityCore/TrinityCore

commit a49544cc187d3a156b4615907d6b82057364fcc4

Creature/Loot: Do not display "lootable" sparkles for player even if others in the group still have quest items to be looted from creature. (#16265)

src/server/game/Loot/LootMgr.cpp
@@ -786,6 +786,15 @@ uint32 Loot::GetMaxSlotInLootFor(Player* player) const
     return items.size() + (itr != PlayerQuestItems.end() ?  itr->second->size() : 0);
 }
 
+// return true if there is any item that is lootable for any player (not quest item, FFA or conditional)
+bool Loot::hasItemForAll() const
+{
+    for (LootItem const& item : items)
+        if (!item.is_looted && !item.freeforall && item.conditions.empty())
+            return true;
+    return false;
+}
+
 // return true if there is any FFA, quest or conditional item for the player.
 bool Loot::hasItemFor(Player* player) const
 {

src/server/game/Entities/Player/Player.cpp
@@ -17571,6 +17571,8 @@ bool Player::isAllowedToLoot(const Creature* creature)
     const Loot* loot = &creature->loot;
     if (loot->isLooted()) // nothing to loot or everything looted.
         return false;
+    if (!loot->hasItemForAll() && !loot->hasItemFor(this)) // no loot in creature for this player
+        return false;
 
     if (loot->loot_type == LOOT_SKINNING)
         return creature->GetSkinner() == GetGUID();

src/server/game/Handlers/LootHandler.cpp
@@ -370,13 +370,10 @@ void WorldSession::DoLootRelease(ObjectGuid lguid)
                 loot->roundRobinPlayer.Clear();
 
                 if (Group* group = player->GetGroup())
-                {
                     group->SendLooter(creature, NULL);
-
-                    // force update of dynamic flags, otherwise other group's players still not able to loot.
-                    creature->ForceValuesUpdateAtIndex(UNIT_DYNAMIC_FLAGS);
-                }
             }
+            // force dynflag update to update looter and lootable info
+            creature->ForceValuesUpdateAtIndex(UNIT_DYNAMIC_FLAGS);
         }
     }
 

src/server/game/Loot/LootMgr.h
@@ -382,6 +382,7 @@ struct TC_GAME_API Loot
 
     LootItem* LootItemInSlot(uint32 lootslot, Player* player, QuestItem** qitem = NULL, QuestItem** ffaitem = NULL, QuestItem** conditem = NULL);
     uint32 GetMaxSlotInLootFor(Player* player) const;
+    bool hasItemForAll() const;
     bool hasItemFor(Player* player) const;
     bool hasOverThresholdItem() const;
 

allankrdec/TrinityCore-10-2024

commit 2fe9f6329e536d88a6e9446dd5f7f30446e7f2f6

Core/Loot: fix bug introduced in a49544cc187d3a156b4615907d6b82057364fcc4 in which creatures only having gold wouldn't show lootable sparks, and thus couldn't be looted

src/server/game/Loot/LootMgr.cpp
@@ -789,6 +789,10 @@ uint32 Loot::GetMaxSlotInLootFor(Player* player) const
 // return true if there is any item that is lootable for any player (not quest item, FFA or conditional)
 bool Loot::hasItemForAll() const
 {
+    // Gold is always lootable
+    if (gold)
+        return true;
+
     for (LootItem const& item : items)
         if (!item.is_looted && !item.freeforall && item.conditions.empty())
             return true;